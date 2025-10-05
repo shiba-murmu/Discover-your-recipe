e15b5083ba0cf178f0da2496de3f32ca242f0b37
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-05 21:21:21 +0530

    demo text is added in the login page

--- frontend/src/pages/Auth/Login/LoginPage.jsx
+++ frontend/src/pages/Auth/Login/LoginPage.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function LoginPage() {
   return (
     <>
-        <div className='h-screen flex justify-center items-center bg-amber-100'>
-           <div>
-                
+        <div className='h-screen flex justify-center items-center bg-gray-900'>
+           <div className='text-white font-bold text-3xl'>
+                login page
            </div>
         </div>
     </>
