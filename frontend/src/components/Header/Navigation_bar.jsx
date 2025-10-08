fd1b5ae7ee460c39b738d0cddcb9ff915eb2200f
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-08 22:38:46 +0530

    cols is added to the navbar,

--- frontend/src/components/Header/Navigation_bar.jsx
+++ frontend/src/components/Header/Navigation_bar.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import DarkModeToggle from '../../utils/Theme/DarkModeToggle'
 
 function Navigation_bar() {
-  return (
-    <>
-        <div className='h-16 text-white bg-[var(--light-dark-green)] dark:bg-[var(--dark-darker-black)] dark:text-[var(--lightest-white)] flex items-center justify-center'>
-            <div className='w-11/12 grid grid-cols-5 gap-4 place-content-center'>
-                <div>Home</div>
-                <div>About</div>
-                <div>Services</div>
-                <div>Contact</div>
-                <div>Login</div>
-                <DarkModeToggle />
+    return (
+        <>
+            <div className='h-16 text-white bg-[var(--light-dark-green)] dark:bg-[var(--dark-darker-black)] dark:text-[var(--lightest-white)] flex items-center justify-center'>
+                <div className='w-11/12 grid grid-cols-6 gap-4 place-content-center'>
+                    <div>Home</div>
+                    <div>About</div>
+                    <div>Services</div>
+                    <div>Contact</div>
+                    <div>Login</div>
+                    <div>
+                        <DarkModeToggle />
+                    </div>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navigation_bar
