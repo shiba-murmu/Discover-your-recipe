f49f9ece79e22da2061e17659ce0fefb56bbe9e3
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-18 16:52:15 +0530

    choose color palette for light theme.

--- frontend/src/pages/Landing/Landing_page.jsx
+++ frontend/src/pages/Landing/Landing_page.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import DarkModeToggle from '../../utils/Theme/DarkModeToggle'
 
 function Landing_page() {
   return (
-    <div>Landing_page</div>
+    <> 
+        <div>
+            {/* <DarkModeToggle /> */}
+            <div className='h-80 md:h-96 w-full bg-[var(--primary-color)] flex flex-col justify-center items-center'>
+                {/* This one is other div */}
+                <img src="" alt="" />
+            </div>
+            <div className='h-96 w-full bg-[var(--secondary-color)] flex flex-col justify-center items-center  '>
+                {/* This one is for other animation division */}
+            </div>
+            <div className='h-80 md:h-96 w-full bg-[var(--background-color)] flex flex-col justify-center items-center'>
+                {/* This one is other division for animation. */}
+            </div>
+            <div className='h-40 w-full bg-[var(--text-color)] flex flex-col justify-center items-center'>
+                {/* aslkdfalksdflasmdf */}
+            </div>
+        </div>
+    </>
   )
 }
 
