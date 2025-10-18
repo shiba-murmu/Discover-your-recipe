069a28c6c0c08d2572e762ca434948203ef0c823
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-18 17:29:49 +0530

    images and some files are added like for landing page image and its css files.

--- frontend/src/pages/Landing/Landing_page.jsx
+++ frontend/src/pages/Landing/Landing_page.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import DarkModeToggle from '../../utils/Theme/DarkModeToggle'
+import food from '../../assets/food.png'
+import food_2 from '../../assets/food2.png'
+import larger_screen from '../../assets/larger_screen_food.jpg'
 
 function Landing_page() {
   return (
     <> 
         <div>
-            {/* <DarkModeToggle /> */}
-            <div className='h-80 md:h-96 w-full bg-[var(--primary-color)] flex flex-col justify-center items-center'>
-                {/* This one is other div */}
-                <img src="" alt="" />
-            </div>
-            <div className='h-96 w-full bg-[var(--secondary-color)] flex flex-col justify-center items-center  '>
-                {/* This one is for other animation division */}
-            </div>
-            <div className='h-80 md:h-96 w-full bg-[var(--background-color)] flex flex-col justify-center items-center'>
-                {/* This one is other division for animation. */}
-            </div>
-            <div className='h-40 w-full bg-[var(--text-color)] flex flex-col justify-center items-center'>
-                {/* aslkdfalksdflasmdf */}
-            </div>
+            {/* For use of image */}
+            <img src={food} alt="Food" className='h-[90vh] md:hidden w-full object-cover' />
+            <img src={larger_screen} className='hidden md:block h-[90vh] w-full object-cover' alt="" />
         </div>
     </>
   )
