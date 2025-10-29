2f222375b3304579501c99dc5d69a7a5adfb6f15
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 16:11:03 +0530

    contact page is updated according to the readme file.

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+        <div className='mt-17 md:mt-16'>
+            contact me
+        </div>
+    </>
   )
 }
 
