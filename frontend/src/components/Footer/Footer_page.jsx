ee055a49ba7979c3cff9c73087cf0be7d656b0a8
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 20:54:06 +0530

    Footer file is created in the website

---
+++ frontend/src/components/Footer/Footer_page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer_page() {
+  return (
+    <>
+        <div>
+            Footer page for the website here design the UI.
+        </div>
+    </>
+  )
+}
+
+export default Footer_page
