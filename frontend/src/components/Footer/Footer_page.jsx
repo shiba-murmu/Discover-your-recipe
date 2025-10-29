61be6d44fb95446c3df0e253e1f949724afcf690
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 21:59:10 +0530

    website image icons as png is added to the page.

--- frontend/src/components/Footer/Footer_page.jsx
+++ frontend/src/components/Footer/Footer_page.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import brand_icons from '../../assets/icons/icons.png'
 function Footer_page() {
     const goToTop = () => {
+        // this function here to scroll to top of the page
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }
     return (
         <>
-            <div id='footer' className='w-full h-[70vh] md:h-[70vh]  bg-[#113d3c] text-white p-4 pt-20'>
+            <div className='w-full h-[70vh] md:h-[70vh]  bg-[#113d3c] text-white p-4 pt-10 md:pt-20'>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                     <div className='flex flex-col gap-4 md:pl-15'>
-                        <div className=' md:pb-5'>
+                        <div className='flex items-center justify-start gap-3 md:pb-5 md:flex md:items-center md:gap-3'>
+                            <img src={brand_icons} className='md:h-10 md:w-10 h-10 object-cover' alt="" />
                             <span className='text-3xl'>FOODIEMEDIA</span>
                         </div>
                         <div className='md:mb-10 '>
-                            <p className='w-97 md:w-96'>
+                            <p className='md:w-96'>
                                 Empowering Food Lovers Everywhere and Connecting Culinary Enthusiasts Worldwide.
                                 People who love food and cooking.
                             </p>
