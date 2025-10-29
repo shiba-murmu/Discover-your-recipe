a81cf1772154e9381c88e91af8347534fda7ce1f
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 15:48:51 +0530

    login page designed. For to login the user into the website.

--- frontend/src/pages/Auth/Login/LoginPage.jsx
+++ frontend/src/pages/Auth/Login/LoginPage.jsx
@@ -4,9 +4,13 @@ import DarkModeToggle from '../../../utils/Theme/DarkModeToggle'
 function LoginPage() {
     return (
         <>
-            <div className='min-h-screen flex items-center justify-center  dark:bg-[var(--universal-dark-bg)]'>
+            <div className='min-h-screen flex items-center flex-col justify-center '>
                 {/* <DarkModeToggle /> */}
-                Loginpage
+                <div>
+                    <h1 className='text-4xl font-extrabold mb-6 text-center'>Welcome to FoodieMedia</h1>
+                    <p className='text-center mb-8'>Login to continue to your foodie journey!</p>
+                </div>
+                <button className='bg-blue-300  px-6 py-2 rounded-md hover:bg-blue-700 hover:text-white transition duration-300'>Login with Google</button>
             </div>
         </>
     )
