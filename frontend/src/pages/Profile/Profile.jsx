c08ca371d410c94ff8022508c85c12c59e07dc1e
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 18:40:29 +0530

    added some more images in the profile page.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -30,12 +30,12 @@ function UserPosts() {
     return (
         <>
             <div className='grid grid-cols-3 md:grid-cols-5 gap-2 md:gap-4 p-2 md:p-4'>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((item) => (
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30].map((item) => (
                     <img
                         key={item}
                         src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
                         alt=""
-                        className="h-30 w-30 md:h-60 md:w-64 rounded-md object-cover transform hover:scale-105 hover:shadow-lg transition-all duration-700 ease-in-out"
+                        className="h-30 w-30 md:h-60 md:w-64 rounded-md object-cover transform hover:scale-105 hover:shadow-lg cursor-pointer  transition-all duration-700 ease-in-out"
                     />
                 ))}
             </div>
