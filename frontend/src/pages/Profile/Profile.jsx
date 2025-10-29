ad5f07bace6901baf1cac480d3fda6d10a936cae
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 15:32:55 +0530

    posts functions items design for the profile page.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -3,21 +3,42 @@ import profile from '../../assets/food.png'
 function Profile() {
     return (
         <>
-          <div className='min-h-screen flex flex-col items-center justify-start'>
-            <div className='h-40 md:h-50 w-full flex items-center justify-center gap-10  md:justify-start' >
-                <img src={profile} className='h-30 w-30 md:h-40 md:w-40 object-cover md:ml-10 border-3 border-green-600 dark:border-amber-50 rounded-full ' alt="" />
-                <div className='hidden md:block'>
-                    <h1 className='text-2xl font-extrabold'>Shiba Murmu</h1>
-                    <h4>Posts  :  5</h4>
+            <div className='min-h-screen flex flex-col items-center justify-start'>
+                <div className='h-40 md:h-50 w-full flex items-center justify-center gap-10  md:justify-start' >
+                    <img src={profile} className='h-30 w-30 md:h-40 md:w-40 object-cover md:ml-10 border-3 border-green-600 dark:border-amber-50 rounded-full ' alt="" />
+                    <div className='hidden md:block'>
+                        <h1 className='text-2xl font-extrabold'>Shiba Murmu</h1>
+                        <h4>Posts  :  5</h4>
+                    </div>
                 </div>
+                <div className='md:hidden mb-5 h-10 flex flex-col'>
+                    <h1 className='text-xl font-extrabold'>Shiba Murmu</h1>
+                    <h3 className='text-center'>Posts : 5</h3>
+                </div>
+                < UserPosts />
             </div>
-            <div className='h-10 flex flex-col'>
-                <h1 className='text-xl font-extrabold'>Shiba Murmu</h1>
-                <h3 className='text-center'>Posts : 5</h3>
-            </div>
-          </div>
         </>
     )
 }
 
 export default Profile;
+
+function UserPosts() {
+    return (
+        <>
+            <div className='grid grid-cols-3 md:grid-cols-5 gap-2 md:gap-4 p-2 md:p-4'>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((item) => (
+                    <img
+                        key={item}
+                        src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                        alt=""
+                        className="h-30 w-30 md:h-60 md:w-50 rounded-md object-cover transform hover:scale-105 hover:shadow-lg transition-all duration-700 ease-in-out"
+
+                    />
+
+                ))}
+            </div>
+
+        </>
+    )
+}
