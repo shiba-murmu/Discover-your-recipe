358dd2deba7bfe6d0311ef3cf935d753a2f522dd
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 21:38:31 +0530

    added images and desiging the footer of the apge.

--- frontend/src/components/Footer/Footer_page.jsx
+++ frontend/src/components/Footer/Footer_page.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 function Footer_page() {
-  return (
-    <>
-        <div>
-            Footer page for the website here design the UI.
-        </div>
-    </>
-  )
+    const goToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+    return (
+        <>
+            <div id='footer' className='w-full h-[70vh] md:h-[70vh]  bg-[#113d3c] text-white p-4 pt-20'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    <div className='flex flex-col gap-4 md:pl-15'>
+                        <div className=' md:pb-5'>
+                            <span className='text-3xl'>FOODIEMEDIA</span>
+                        </div>
+                        <div className='md:mb-10 '>
+                            <p className='w-97 md:w-96'>
+                                Empowering Food Lovers Everywhere and Connecting Culinary Enthusiasts Worldwide.
+                                People who love food and cooking.
+                            </p>
+                        </div>
+                        <div className='flex gap-4 md:mb-10'>
+                            Social media icons
+                        </div>
+                        <div>
+                            <button onClick={goToTop} className='bg-[#256D6B] px-4 py-2 rounded-md hover:bg-[#1e5554] cursor-pointer'>
+                                back to top
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            some more options will be added here
+                        </div>
+                        <div>
+                            Legal terms and conditions privacy policy
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Footer_page
--- frontend/src/pages/Home/Home_page.jsx
+++ frontend/src/pages/Home/Home_page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home_page() {
   return (
    <>
-        <div className='mt-17 md:mt-16'>
+        <div className='mt-17 md:mt-16 min-h-[100vh]'>
             Home page
         </div>
    </>
