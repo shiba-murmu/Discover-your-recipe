72958108835c532dae85fc2e22f7706bc4aa2f00
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 16:16:05 +0530

    Recipe page is also updated.

--- frontend/src/pages/Recipes/Recipes_post.jsx
+++ frontend/src/pages/Recipes/Recipes_post.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Recipes_post() {
   return (
     <>
-        <div className=''>
-            Recipe posts added in the printer.
+        <div className='mt-17 md:mt-16'>
+            Recipe page is also updated.
         </div>
     </>
   )
