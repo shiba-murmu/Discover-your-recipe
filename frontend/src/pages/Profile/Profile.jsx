332fc49316ae26e0b02b18868102783aee153720
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 15:38:22 +0530

    profile styles designed here,

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -4,8 +4,8 @@ function Profile() {
     return (
         <>
             <div className='min-h-screen flex flex-col items-center justify-start'>
-                <div className='h-40 md:h-50 w-full flex items-center justify-center gap-10  md:justify-start' >
-                    <img src={profile} className='h-30 w-30 md:h-40 md:w-40 object-cover md:ml-10 border-3 border-green-600 dark:border-amber-50 rounded-full ' alt="" />
+                <div className='h-40 md:h-80 w-full flex items-center justify-center gap-10  md:justify-start' >
+                    <img src={profile} className='h-30 w-30 md:h-40 md:w-40 object-cover md:ml-50 border-3 border-green-600 dark:border-amber-50 rounded-full ' alt="" />
                     <div className='hidden md:block'>
                         <h1 className='text-2xl font-extrabold'>Shiba Murmu</h1>
                         <h4>Posts  :  5</h4>
