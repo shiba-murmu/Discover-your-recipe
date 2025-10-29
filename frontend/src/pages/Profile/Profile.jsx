62459c47a8669f44ae502a68051ea1834a744ae3
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-29 14:42:12 +0530

    desigend for profile page.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import profile from '../../assets/food.png'
 function Profile() {
   return (
-    <div>Profile</div>
+    <>
+        <div className='flex items-center justify-center h-30 md:h-70'>
+            {/* For profile image place */}
+            <div className='flex justify-center items-center h-full mt-20 md:mt-0 text-3xl font-bold'>
+                <img src={profile} className='h-30 w-30 md:h-40 md:w-40 object-center rounded-full ' alt="" />
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </>
   )
 }
 
