63bc845484356a8466a75a99f939d9e827775ada
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-30 14:29:35 +0530

    Profile name changed.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -7,7 +7,7 @@ function Profile() {
                 <div className='h-40 md:h-80 w-full flex items-center justify-center gap-10  md:justify-start' >
                     <img src={profile} className='h-30 w-30 md:h-40 md:w-40 object-cover md:ml-50 border-3 border-green-600 dark:border-amber-50 rounded-full ' alt="" />
                     <div className='hidden md:block'>
-                        <h1 className='text-2xl font-extrabold'>Shiba Murmu</h1>
+                        <h1 className='text-2xl font-extrabold'>No Name </h1>
                         <h4>Posts  :  5</h4>
                     </div>
                 </div>
