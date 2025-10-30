9ff26c668f713c17abe275dd6038c70e7f1e37f4
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-30 13:42:34 +0530

    Height is set for the page.

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Contact() {
   return (
     <>
-        <div className='mt-17 md:mt-16'>
+        <div className='min-h-[100vh] mt-17 md:mt-16'>
             contact me
         </div>
     </>
