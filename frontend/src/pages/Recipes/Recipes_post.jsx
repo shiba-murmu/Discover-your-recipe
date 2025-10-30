c151821f5e2318faa78159897a595586eb4972d2
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-30 13:47:44 +0530

    min  height set for the recipe page

--- frontend/src/pages/Recipes/Recipes_post.jsx
+++ frontend/src/pages/Recipes/Recipes_post.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Recipes_post() {
   return (
     <>
-        <div className='mt-17 md:mt-16'>
+        <div className='min-h-[100vh] mt-17 md:mt-16'>
             <div>
                 Headings for recipes from different part of the world
             </div>
