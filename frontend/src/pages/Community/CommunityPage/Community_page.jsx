2285460b8db4ff23e2fd306fbf080c27ca90de80
Shiba Murmu <shibamurmu001@gmail.com>
2025-10-30 13:48:23 +0530

    min height set for the community page.

--- frontend/src/pages/Community/CommunityPage/Community_page.jsx
+++ frontend/src/pages/Community/CommunityPage/Community_page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Community_page() {
   return (
    <>
-        <div className='mt-17 md:mt-16'>
+        <div className='min-h-[100vh] mt-17 md:mt-16'>
                 community page is also updated 
         </div>
    </>
