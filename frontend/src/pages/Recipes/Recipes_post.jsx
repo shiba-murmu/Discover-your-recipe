b74cae5a710dc92a02996feb62e3b373f928d396
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-13 18:26:08 +0530

    card designed for the recipe to show the use according to user preferences.

--- frontend/src/pages/Recipes/Recipes_post.jsx
+++ frontend/src/pages/Recipes/Recipes_post.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Recipe_post.css';
 // import img from '../../assets/image/food.png';
+import image from '../../assets/large_food_2.jpg';
 
 function Recipes_post() {
     // Recipies from different regions will show here.,
     return (
         <>
             <div className='min-h-[100vh] mt-17 md:mt-16'>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-2 p-0 md:p-4" >
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 md:gap-2 p-0 md:p-4" >
                     <div className="h-[30vh] w-full ">
                         <RecipeCard />
                     </div>
@@ -22,8 +24,12 @@ export default Recipes_post
 
 function RecipeCard() {
     return (
-        <div className="h-[30vh] w-full  border" style={{ backgroundImage: `url()` }}>
-            Recipe card
+        <div className="h-[30vh] w-full text-white flex items-end justify-center" style={{ backgroundImage: `url(${image})` , backgroundSize: 'cover', backgroundPosition: 'center'  }}>
+            
+            <div className="card_background flex flex-col justify-end h-full w-full p-2 ">
+                <h1 className="text-xl font-bold">Bengal Recipe</h1>
+                <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+            </div>
         </div>
     )
 }
