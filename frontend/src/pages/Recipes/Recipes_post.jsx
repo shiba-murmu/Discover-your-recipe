7de344cc13bd861b556f41b3f311d99c01d84bd4
Shiba Murmu <shibamurmu001@gmail.com>
2025-11-13 17:58:27 +0530

    New recipe page is going to design.

--- frontend/src/pages/Recipes/Recipes_post.jsx
+++ frontend/src/pages/Recipes/Recipes_post.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
+// import img from '../../assets/image/food.png';
 
 function Recipes_post() {
-  return (
-    <>
-        <div className='min-h-[100vh] mt-17 md:mt-16'>
-            <div>
-                Headings for recipes from different part of the world
+    // Recipies from different regions will show here.,
+    return (
+        <>
+            <div className='min-h-[100vh] mt-17 md:mt-16'>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-2 p-0 md:p-4" >
+                    <div className="h-[30vh] w-full ">
+                        <RecipeCard />
+                    </div>
+                    <div className="h-[30vh] w-full bg-red-800 "></div>
+                    <div className="h-[30vh] w-full bg-blue-800 "></div>
+                </div>
             </div>
-            <div>
-                container for the particulat recipes.
-            </div>
-            
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Recipes_post
+
+function RecipeCard() {
+    return (
+        <div className="h-[30vh] w-full  border" style={{ backgroundImage: `url()` }}>
+            Recipe card
+        </div>
+    )
+}
